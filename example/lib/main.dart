2c52bfba323aae671feaa9eaeed2d17fd9ff569c
jushutch <jushutch@umich.edu>
2021-10-14 00:15:20 -0400

    Add example application

---
+++ example/lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:swiping_card_deck/swiping_card_deck.dart';
+import 'dart:math' as math;
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: ExamplePage(),
+        )
+      ),
+      title: 'SwipingCardDeck',
+    )
+  );
+}
+
+class ExamplePage extends StatelessWidget {
+  const ExamplePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final SwipingCardDeck deck = SwipingCardDeck(
+      cardDeck: getCardDeck(),
+      onDeckEmpty: () => debugPrint("Card deck empty"),
+      onLeftSwipe: (Card card) => debugPrint("Swiped left!"),
+      onRightSwipe: (Card card) => debugPrint("Swiped right!"),
+      cardWidth: 200,
+      swipeThreshold: MediaQuery.of(context).size.width / 3,
+      minimumVelocity: 1000,
+    );
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        deck,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+              icon: const Icon(Icons.clear),
+              iconSize: 30,
+              color: Colors.red,
+              onPressed: deck.animationActive ? null : () => deck.swipeLeft(MediaQuery.of(context).size),
+            ),
+            const SizedBox(width: 40),
+            IconButton(
+              icon: const Icon(Icons.check),
+              iconSize: 30,
+              color: Colors.green,
+              onPressed: deck.animationActive ? null : () => deck.swipeRight(MediaQuery.of(context).size),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  List<Card> getCardDeck() {
+    List<Card> cardDeck = [];
+    for (int i = 0; i < 500; ++i) {
+      cardDeck.add(
+        Card(
+          color: Color((math.Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0),
+          child: const SizedBox(height: 300, width: 200)
+        ),
+      );
+    }
+    return cardDeck;
+  }
+  
+}
