cb921bcd72d4c43557f556a8d9317433b6972a31
jushutch <jushutch@umich.edu>
2022-02-09 12:41:52 -0500

    Update example with rotationFactor and swipeAnimationDuration

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -25,6 +25,8 @@ class ExamplePage extends StatelessWidget {
       cardWidth: 200,
       swipeThreshold: MediaQuery.of(context).size.width / 3,
       minimumVelocity: 1000,
+      rotationFactor: 0.8 / 3.14,
+      swipeAnimationDuration: const Duration(milliseconds: 500),
     );
     return Column(
       mainAxisSize: MainAxisSize.min,
@@ -39,18 +41,14 @@ class ExamplePage extends StatelessWidget {
               icon: const Icon(Icons.clear),
               iconSize: 30,
               color: Colors.red,
-              onPressed: deck.animationActive
-                  ? null
-                  : () => deck.swipeLeft(),
+              onPressed: deck.animationActive ? null : () => deck.swipeLeft(),
             ),
             const SizedBox(width: 40),
             IconButton(
               icon: const Icon(Icons.check),
               iconSize: 30,
               color: Colors.green,
-              onPressed: deck.animationActive
-                  ? null
-                  : () => deck.swipeRight(),
+              onPressed: deck.animationActive ? null : () => deck.swipeRight(),
             ),
           ],
         ),
@@ -63,9 +61,9 @@ class ExamplePage extends StatelessWidget {
     for (int i = 0; i < 500; ++i) {
       cardDeck.add(
         Card(
-          color: Color((math.Random().nextDouble() * 0xFFFFFF).toInt())
-              .withOpacity(1.0),
-          child: const SizedBox(height: 300, width: 200)),
+            color: Color((math.Random().nextDouble() * 0xFFFFFF).toInt())
+                .withOpacity(1.0),
+            child: const SizedBox(height: 300, width: 200)),
       );
     }
     return cardDeck;
