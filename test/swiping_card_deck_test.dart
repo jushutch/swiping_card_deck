d2152567916123c2fc6305df15217932fcdcd7b5
jushutch <jushutch@umich.edu>
2022-02-10 23:11:04 -0500

    Include cardDeck and cardsSwiped in swipe callback functions

--- test/swiping_card_deck_test.dart
+++ test/swiping_card_deck_test.dart
@@ -10,9 +10,9 @@ void main() {
     for (int i = 0; i < numCards; ++i) {
       cardDeck.add(
         Card(
-          color: Color((math.Random().nextDouble() * 0xFFFFFF).toInt())
-              .withOpacity(1.0),
-          child: const SizedBox(height: 300, width: 200)),
+            color: Color((math.Random().nextDouble() * 0xFFFFFF).toInt())
+                .withOpacity(1.0),
+            child: const SizedBox(height: 300, width: 200)),
       );
     }
     return cardDeck;
@@ -24,11 +24,17 @@ void main() {
     SwipingCardDeck mockDeck = SwipingCardDeck(
       cardDeck: cardDeck,
       onDeckEmpty: () => debugPrint("Card deck empty"),
-      onLeftSwipe: (dynamic card) => debugPrint("Swiped left!"),
-      onRightSwipe: (dynamic card) => debugPrint("Swiped right!"),
+      onLeftSwipe: (Card card, List<Card> cardDeck, int cardsSwiped) =>
+          debugPrint("Swiped left!"),
+      onRightSwipe: (Card card, List<Card> cardDeck, int cardsSwiped) =>
+          debugPrint("Swiped right!"),
       cardWidth: 200,
     );
-    await tester.pumpWidget(MaterialApp(home: Scaffold(body: mockDeck,),));
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: mockDeck,
+      ),
+    ));
   }
 
   testWidgets("SwipingCardDeck widget is created", (WidgetTester tester) async {
@@ -43,13 +49,14 @@ void main() {
     expect(cardFinder, findsNWidgets(2));
   });
 
-  testWidgets("swipeLeft removes top card and runs callback", (WidgetTester tester) async {
+  testWidgets("swipeLeft removes top card and runs callback",
+      (WidgetTester tester) async {
     await _mountWidget(tester);
     Finder cardFinder = find.byType(Card);
     expect(cardFinder, findsNWidgets(2));
     expect(tester.widget(cardFinder.first) as Card, cardDeck[1]);
     expect(tester.widget(cardFinder.last) as Card, cardDeck[0]);
-    
+
     Finder deckFinder = find.byType(SwipingCardDeck);
     expect(deckFinder, findsOneWidget);
 
@@ -61,7 +68,8 @@ void main() {
     expect(tester.widget(cardFinder.last) as Card, cardDeck[1]);
   });
 
-  testWidgets("swipeRight removes top card and runs callback", (WidgetTester tester) async {
+  testWidgets("swipeRight removes top card and runs callback",
+      (WidgetTester tester) async {
     await _mountWidget(tester);
     Finder deckFinder = find.byType(SwipingCardDeck);
     expect(deckFinder, findsOneWidget);
@@ -78,7 +86,8 @@ void main() {
     expect(tester.widget(cardFinder.last) as Card, cardDeck[1]);
   });
 
-  testWidgets("Callback function is ran when deck is empty", (WidgetTester tester) async {
+  testWidgets("Callback function is ran when deck is empty",
+      (WidgetTester tester) async {
     await _mountWidget(tester);
     Finder deckFinder = find.byType(SwipingCardDeck);
     expect(deckFinder, findsOneWidget);
@@ -93,7 +102,8 @@ void main() {
     expect(cardFinder, findsOneWidget);
     expect(tester.widget(cardFinder.first) as Card, cardDeck[numCards - 1]);
 
-    expectLater(() => deck.swipeLeft(), prints("Swiped left!\nCard deck empty\n"));
+    expectLater(
+        () => deck.swipeLeft(), prints("Swiped left!\nCard deck empty\n"));
     await tester.pumpAndSettle();
 
     expect(cardFinder, findsNothing);
